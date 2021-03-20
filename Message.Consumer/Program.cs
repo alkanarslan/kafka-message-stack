b0e1b1e1df7d59edd855c8be60d0541c5b1c8c6d
Alkan ARSLAN <alkanarslan@gmail.com>
2021-03-20 13:08:20 +0300

    first init

---
+++ Message.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace Message.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Consumer");
+            var topicName = "dev-topic";
+            var kafkaUrl = "192.168.2.184:9092";
+            var config = new ConsumerConfig {GroupId = "group1", BootstrapServers = kafkaUrl};
+            using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe(topicName);
+                while (true)
+                {
+                    var consumeResult = consumer.Consume();
+                    //Mail gonderme, Notifiction Gonderme vs gibi işler yapab bir bussinnes düşünelim.
+                    
+                    Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Message.Value}");
+                    consumer.Commit();
+                }
+            }
+        }
+    }
+}
---
+++ Message.Producer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace Message.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var topicName = "dev-topic";
+            var kafkaUrl = "192.168.2.184:9092";
+            var config = new ProducerConfig() { BootstrapServers = kafkaUrl, };
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.WriteLine("Enter Message: ");
+                    var text = Guid.NewGuid().ToString();
+                    var message = new Message<Null, string> { Value = text };
+                    var result = producer.ProduceAsync(topicName, message).GetAwaiter().GetResult();
+                    Console.WriteLine($"Delivered to : {result.TopicPartitionOffset}");
+                }
+            }
+        }
+    }
+}
